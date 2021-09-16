d7351c88b81e022bef20a33301677cd112b270f8
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2021-09-16 17:23:24 +0100

    Addition of first js code

---
+++ 03-JavaScript-Fundamentals/calculator-challenge/script.js
@@ -0,0 +1,9 @@
+function getOutput() {
+  return document.getElementById("output-value").innerText;
+}
+function printOutput(num) {
+  document.getElementById("output-valie").innerText = num;
+}
+printOutput("1234");
+
+alert(getOutput("22"));
