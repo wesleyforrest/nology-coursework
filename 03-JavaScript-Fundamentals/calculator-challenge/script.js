aa25bfc0d04565703a0663fefe316fc7684d9e93
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2021-09-17 13:52:33 +0100

    More buttin styling

--- 03-JavaScript-Fundamentals/calculator-challenge/script.js
+++ 03-JavaScript-Fundamentals/calculator-challenge/script.js
@@ -13,4 +13,29 @@ function getFormattedNumber(num) {
   let value = n.toLocaleString("en");
   return value;
 }
-printOutput("9977");
+function reverseNumberFormat(num) {
+  return number(num.replace(/,/g, ""));
+}
+
+const operator = document.getElementsByClassName("operator");
+console.log(operator);
+for (let i = 0; i < operator.length; i++) {
+  operator[i].addEventListener("click", function (event) {
+    let operatoerValue = operator[i].innerText;
+    console.log(operatoerValue);
+  });
+}
+
+let number = document.getElementsByClassName("number");
+for (let i = 0; i < number.length; i++) {
+  number[i].addEventListener("click", function () {
+    let initialValue = parseFloat(number[i].innerText);
+    document.querySelector("#output-value").innerText = initialValue;
+    /*let output = reverseNumberFormat(getOutput());
+    if (output != NaN) {
+      output = output + this.id;
+      printOutput(output);
+    }
+    */
+  });
+}
