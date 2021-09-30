35f525b5f0ef122be7b4591cda1cb9cf8530d342
wesleyforrest <79322205+wesleyforrest@users.noreply.github.com>
2021-09-30 10:26:37 +0100

    Object lesson complete

--- 03-JavaScript-Fundamentals/objects/script.js
+++ 03-JavaScript-Fundamentals/objects/script.js
@@ -1,21 +1,40 @@
 //
 
+const studentContainer = document.querySelector(".students-container");
+
+//studentContainer.style.border = "50px solid red";
+
 const person = {
   firstName: "Charlie",
   lastName: "Richardson",
   quote: "It doesn't get easier, you just go faster",
   skills: ["html", "css", "JavaScript"],
+  imgURL:
+    "https://www.searchpng.com/wp-content/uploads/2019/02/Deafult-Profile-Pitcher.png",
   getFullName() {
     return `${this.firstName} ${this.lastName}`;
   },
   skillsList() {
     const skillsList = this.skills.map((skill) => `<li>${skill}</li>`);
-    console.log(skillsList);
     return skillsList.join("\n");
+  },
+  getPersonHTML() {
+    const personHTML = `<article class="student-card">
+  <img src=${this.imgURL} />
+  <h2>${this.getFullName()}</h2>
+  <blockquote>${this.quote}</blockquote>
+  <h3>Skills</h3>
+  <ul>${this.skillsList()}</ul>
+</article>
+`;
+    return personHTML;
   },
 };
 
-console.log(person.skillsList());
+for (let i = 0; i < 10; i++) {
+  studentContainer.innerHTML += person.getPersonHTML();
+}
+
 // person.age = 30;
 
 // console.log(person.firstName + " " + person.lastName);
